4bb63ca11575173c4e962d25dd43d367e61162db
ghostbuster91 <kghost0@gmail.com>
2018-06-11 12:16:25 +0200

    Fix not returning first item

--- src/main/java/io/userfeeds/Viewer.kt
+++ src/main/java/io/userfeeds/Viewer.kt
@@ -11,9 +11,16 @@ class Viewer(private val store: Store) {
     @RequestMapping("/purrs", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getPurrs(@RequestParam("after", required = false) after: String?, @RequestParam("size") size: Int): Page {
         val totalPages = store.cache.size / size
-        val items = store.cache.dropWhile { if(after != null ) it["id"] as String != after else false }.drop(1).take(size)
+        val items = getItems(after, size)
         return Page(totalPages = totalPages, total = store.cache.size, items = items)
     }
+
+    private fun getItems(after: String?, size: Int) =
+            if (after != null) {
+                store.cache.dropWhile { it["id"] as String != after }.drop(1).take(size)
+            } else {
+                store.cache.take(size)
+            }
 }
 
 data class Page(val items: List<Any>, val totalPages: Int, val total: Int)
