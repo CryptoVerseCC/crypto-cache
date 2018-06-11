4926b12d9c1d67275e5597dc31f56056b10ebd90
ghostbuster91 <kghost0@gmail.com>
2018-06-11 12:39:03 +0200

    Add before option

--- src/main/java/io/userfeeds/Viewer.kt
+++ src/main/java/io/userfeeds/Viewer.kt
@@ -1,6 +1,6 @@
 package io.userfeeds
 
-import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,19 +8,27 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class Viewer(private val store: Store) {
 
-    @RequestMapping("/purrs", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getPurrs(@RequestParam("after", required = false) after: String?, @RequestParam("size") size: Int): Page {
-        val totalPages = store.cache.size / size
-        val items = getItems(after, size)
+    @RequestMapping("/purrs")
+    @CrossOrigin(origins = ["*"])
+    fun getPurrs(@RequestParam("after", required = false) after: String?,
+                 @RequestParam("size", required = false) size: Int?,
+                 @RequestParam("before", required = false) before: String?): Page {
+        val items = when {
+            after != null && size != null -> getItemsAfter(after, size)
+            before != null -> getItemsBefore(after)
+            size != null -> getXItems(size)
+            else -> throw IllegalArgumentException("After, size  or before must be provided!")
+        }
+        val totalPages = if (size != null) store.cache.size / size else 1
         return Page(totalPages = totalPages, total = store.cache.size, items = items)
     }
 
-    private fun getItems(after: String?, size: Int) =
-            if (after != null) {
-                store.cache.dropWhile { it["id"] as String != after }.drop(1).take(size)
-            } else {
-                store.cache.take(size)
-            }
+    private fun getItemsBefore(after: String?) = store.cache.reversed().takeWhile { it["id"] as String != after }
+
+    private fun getXItems(size: Int) = store.cache.take(size)
+
+    private fun getItemsAfter(after: String?, size: Int) =
+            store.cache.dropWhile { it["id"] as String != after }.drop(1).take(size)
 }
 
 data class Page(val items: List<Any>, val totalPages: Int, val total: Int)
