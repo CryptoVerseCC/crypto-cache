2264bf1c76d324c1efc55476b211b1913f48d9f0
Karol Kowalski <ordon1313@gmail.com>
2018-06-12 11:11:27 +0200

    [Fetch] Fetch full feed

--- src/main/java/io/userfeeds/cachepurr/CryptoPurrApi.kt
+++ src/main/java/io/userfeeds/cachepurr/CryptoPurrApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface CryptoPurrApi {
 
-    @GET("cryptopurr_feed;context=ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d")
+    @GET("cryptopurr_feed;context=ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d;full=true")
     fun getPurrs() : Observable<ItemsWrapper>
 }
 
