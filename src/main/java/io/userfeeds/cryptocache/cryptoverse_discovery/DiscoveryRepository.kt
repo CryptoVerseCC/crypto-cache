0c9ee8a6ac12b3b7760028fd794e39b833cbc519
Maciej Górski <maciek.gorski@gmail.com>
2018-07-27 10:44:46 +0200

    Add MANA, Unicorn and BEN.

--- src/main/java/io/userfeeds/cryptocache/cryptoverse_discovery/DiscoveryRepository.kt
+++ src/main/java/io/userfeeds/cryptocache/cryptoverse_discovery/DiscoveryRepository.kt
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Component
 class DiscoveryRepository {
 
     val assets = mutableSetOf(
-            "ethereum:0xfa6f7881e52fdf912c4a285d78a3141b089ce859"/*,
-            "ethereum:0xe41d2489571d322189246dafa5ebde1f4699f498",
-            "ethereum:0xd26114cd6ee289accf82350c8d8487fedb8a0c07"*/
+            "ethereum:0xfa6f7881e52fdf912c4a285d78a3141b089ce859",
+            "ethereum:0x108c05cac356d93b351375434101cfd3e14f7e44",
+            "ethereum:0x0f5d2fb29fb7d3cfee444a200298f468908cc942",
+            "ethereum:0x89205a3a3b2a69de6dbf7f01ed13b2108b2c43e7"
     )
 
     val discoveries = mutableMapOf<String, Discovery>()
