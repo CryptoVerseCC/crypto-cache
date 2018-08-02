656be87425f4443eac8c4e11ed531885711aa8c6
Maciej Górski <maciek.gorski@gmail.com>
2018-08-02 13:08:08 +0200

    Add Axie to cache.

--- src/main/java/io/userfeeds/cryptocache/cryptoverse_discovery/DiscoveryRepository.kt
+++ src/main/java/io/userfeeds/cryptocache/cryptoverse_discovery/DiscoveryRepository.kt
@@ -14,6 +14,7 @@ class DiscoveryRepository {
 
     val assets = mapOf(
             "ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d" to erc721, // CK
+            "ethereum:0xf5b0a3efb8e8e4c201e2a935f110eaaf3ffecb8d" to erc721, // Axie
             "ethereum:0xf7a6e15dfd5cdd9ef12711bd757a9b6021abf643" to erc721, // Bots
             "ethereum:0xa6d954d08877f8ce1224f6bfb83484c7d3abf8e9" to erc721, // Moji
             "ethereum:0x323a3e1693e7a0959f65972f3bf2dfcb93239dfe" to erc721, // DAC
