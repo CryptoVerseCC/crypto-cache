d1d5ac993e683aad0e759a73261345e065483b2e
Maciej Górski <maciek.gorski@gmail.com>
2018-08-03 12:43:39 +0200

    Add external info to cache api responses.

--- src/main/java/io/userfeeds/cryptocache/opensea/ContextInfoApiModel.kt
+++ src/main/java/io/userfeeds/cryptocache/opensea/ContextInfoApiModel.kt
@@ -2,9 +2,10 @@ package io.userfeeds.cryptocache.opensea
 
 data class ContextInfoApiModel(
         val background_color: String?,
+        val external_link: String,
         val image_url: String,
         val name: String?
 ) {
     constructor(openSeaData: OpenSeaData) :
-            this(openSeaData.backgroundColor, openSeaData.imageUrl, openSeaData.name)
+            this(openSeaData.backgroundColor, openSeaData.externalLink, openSeaData.imageUrl, openSeaData.name)
 }
