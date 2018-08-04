b1ba5613102fb3eff299de2ffbdec6cb698725dd
ghostbuster91 <kghost0@gmail.com>
2018-08-04 22:40:37 +0200

    Push spring configuration to the limits

--- src/main/java/io/userfeeds/cryptocache/CryptoCacheApp.kt
+++ src/main/java/io/userfeeds/cryptocache/CryptoCacheApp.kt
@@ -1,5 +1,6 @@
 package io.userfeeds.cryptocache
 
+import io.userfeeds.cryptocache.opensea.OpenSeaConfig
 import io.userfeeds.cryptocache.opensea.OpenSeaDecoratorAnnotationProcessor
 import io.userfeeds.cryptocache.opensea.OpenSeaItemInterceptor
 import io.userfeeds.cryptocache.retrofit.EnableAutoRetrofit
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.config.AutowireCapableBeanFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
@@ -20,12 +23,14 @@ import java.util.concurrent.TimeUnit
 @SpringBootApplication
 @EnableScheduling
 @EnableAutoRetrofit
+@Import(OpenSeaConfig::class)
 class CryptoCacheApp {
 
     @Bean
     fun taskScheduler(): TaskScheduler = ThreadPoolTaskScheduler().apply { poolSize = 8 }
 
     @Bean
+    @Primary
     fun retrofit() = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
---
+++ src/main/java/io/userfeeds/cryptocache/opensea/OpenSeaConfig.kt
@@ -0,0 +1,27 @@
+package io.userfeeds.cryptocache.opensea
+
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.Executors
+
+@Configuration
+class OpenSeaConfig {
+
+    @Bean(name = ["openSea"])
+    fun retrofit() = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.from(Executors.newFixedThreadPool(10))))
+            .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.HEADERS }).build())
+            .baseUrl("https://opensea-api.herokuapp.com/")
+            .build()
+
+    @Bean
+    fun api(@Qualifier("openSea") retrofit: Retrofit) = retrofit.create(OpenSeaApi::class.java)
+}
