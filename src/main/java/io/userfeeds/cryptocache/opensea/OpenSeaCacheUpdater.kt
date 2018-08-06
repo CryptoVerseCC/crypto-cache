9b614e7aaef71e496aab630a7539ff75fc04561a
ghostbuster91 <kghost0@gmail.com>
2018-08-06 11:25:06 +0200

    Restore scheduled job of OpenSeaCacheUpdater

--- src/main/java/io/userfeeds/cryptocache/opensea/OpenSeaCacheUpdater.kt
+++ src/main/java/io/userfeeds/cryptocache/opensea/OpenSeaCacheUpdater.kt
@@ -1,6 +1,7 @@
 package io.userfeeds.cryptocache.opensea
 
 import io.reactivex.rxkotlin.toObservable
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,7 +11,7 @@ class OpenSeaCacheUpdater(
         private val service: OpenSeaService
 ) {
 
-    //@Scheduled(fixedDelay = 8 * 3600 * 1000, initialDelay = 3600 * 1000)
+    @Scheduled(fixedDelay = 8 * 3600 * 1000, initialDelay = 3600 * 1000)
     fun updateCache() {
         val newItems = repository.findAll()
                 .map(OpenSeaData::asset)
