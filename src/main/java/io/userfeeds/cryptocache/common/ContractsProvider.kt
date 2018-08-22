bb2a6f6795e4c62180fabadd7d4f060986ad53c2
Karol Kowalski <ordon1313@gmail.com>
2018-08-22 11:54:47 +0200

    Add update contracts mapping endpoint

--- src/main/java/io/userfeeds/cryptocache/common/ContractsProvider.kt
+++ src/main/java/io/userfeeds/cryptocache/common/ContractsProvider.kt
@@ -1,14 +1,55 @@
 package io.userfeeds.cryptocache.common
 
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import io.userfeeds.contractmapping.CONTRACTS
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicReference
 
 @Component
-class ContractsProvider {
+class ContractsProvider(moshiConverterFactory: MoshiConverterFactory) {
+    private val contractMapping = Retrofit.Builder()
+            .addConverterFactory(moshiConverterFactory)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.from(Executors.newFixedThreadPool(10))))
+            .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.HEADERS }).build())
+            .baseUrl("https://raw.githubusercontent.com")
+            .build()
+            .create(ContractMapping::class.java)
+
+    private val contracts: AtomicReference<List<Contract>> = AtomicReference(CONTRACTS.ALL.map { Contract(it.network, it.address, it.name, it.symbol, it.is721) })
+
+    @GetMapping("update_contract_mapping")
+    fun update(): String {
+        try {
+            val currentContracts = contractMapping.getCurrentMapping().blockingGet().values.toList()
+            currentContracts.forEach {
+                if(it.address != it.address.toLowerCase()) {
+                    throw RuntimeException("Patryk!!! miało być lowercasem co to jest ${it.address} w ${it.name}")
+                }
+            }
+            contracts.set(currentContracts)
+            return "Patryk wszystko poszło ok"
+        } catch (e: Exception) {
+            return "Patryk coś zjebałeś. Pokaż to Maćkowi jak nie wiesz czemu nie działa ${e.message ?: e.toString()}"
+        }
+    }
 
     fun get(): List<Contract> {
-        return CONTRACTS.ALL.map { Contract(it.network, it.address, it.name, it.symbol, it.is721) }
+        return contracts.get()
     }
+}
+
+interface ContractMapping {
+    @GET("CryptoVerseCC/contract-mapping/master/mapping.json")
+    fun getCurrentMapping(): Single<Map<String, Contract>>
 }
 
 data class Contract(
---
+++ src/test/java/io/userfeeds/cryptocache/CryptoCacheAppTest.kt
@@ -0,0 +1,37 @@
+package io.userfeeds.cryptocache
+
+import com.palantir.docker.compose.DockerComposeRule
+import io.userfeeds.cryptocache.common.ContractsProvider
+import org.assertj.core.api.Assertions.*
+import org.junit.ClassRule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+
+@SpringBootTest(classes = [CryptoCacheApp::class])
+@RunWith(SpringJUnit4ClassRunner::class)
+class CryptoCacheAppTest {
+
+    companion object {
+        @ClassRule
+        @JvmField
+        val docker: DockerComposeRule = DockerComposeRule.builder()
+                .file("src/test/resources/docker-compose.yml")
+                .build()
+    }
+
+    @Autowired
+    lateinit var contractsProvider: ContractsProvider
+
+    @Test
+    fun shouldPass() = Unit
+
+    @Test
+    fun shouldUpdateContractMapping() {
+        assertThat(contractsProvider.update())
+                .isEqualTo("Patryk wszystko poszło ok")
+    }
+}
